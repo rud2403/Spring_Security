085d8b3a66121e3f4bb1596ab452eafa5a79a10c
bgm <qkrrudals2403@naver.com>
2023-01-18 14:40:31 +0900

    feat : create HelloController

---
+++ ssia-2-1/src/main/java/com/laurentiuspilca/ssia21/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.laurentiuspilca.ssia21.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
